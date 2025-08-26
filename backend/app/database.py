e34df5ab48a714929070791c88db76c360dfb400
Marcos Napolitano <marcos.sebastian.napolitano@gmail.com>
2025-08-26 12:08:27 -0300

    Update database.py
    Correccion para compatibilidad con render

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,20 +2,24 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
 
-# Leemos las variables de entorno para construir la URL de conexión
-DB_USER = os.environ.get("POSTGRES_USER")
-DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-DB_NAME = os.environ.get("POSTGRES_DB")
-DB_HOST = "db" # Este es el nombre del servicio en docker-compose.yml
+# Leemos la única variable de entorno que nos dará Render
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+# Si estamos en local (Render no define esta URL), usamos nuestras variables .env
+if not SQLALCHEMY_DATABASE_URL:
+    DB_USER = os.environ.get("POSTGRES_USER")
+    DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    DB_NAME = os.environ.get("POSTGRES_DB")
+    DB_HOST = "db"
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency to get DB session
+# Dependencia para la Sesión de la Base de Datos
 def get_db():
     db = SessionLocal()
     try:
