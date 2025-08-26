094afbf10e85f50e54f276b7cc03c88feb7c3dd5
Marcos Napolitano <marcos.sebastian.napolitano@gmail.com>
2025-08-26 12:14:50 -0300

    Update database.py
    Version compatible con Render.yml

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,25 +1,27 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import text
+from sqlalchemy.ext.declarative import declarative_base
 
-# Leemos la única variable de entorno que nos dará Render
+# Render nos dará esta variable de entorno automáticamente.
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
-# Si estamos en local (Render no define esta URL), usamos nuestras variables .env
+# Si la variable no existe (porque estamos en local), la construimos
+# con nuestro archivo .env como antes.
 if not SQLALCHEMY_DATABASE_URL:
-    DB_USER = os.environ.get("POSTGRES_USER")
+    DB_USER = os.environ.get("POSTG-RES_USER")
     DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
     DB_NAME = os.environ.get("POSTGRES_DB")
     DB_HOST = "db"
     SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
-# Dependencia para la Sesión de la Base de Datos
+# Dependencia para la sesión de la base de datos
 def get_db():
     db = SessionLocal()
     try:
